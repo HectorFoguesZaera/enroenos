8ab08f9dfdb8b248d240cb08b56c4deb4092700b
HectorFoguesZaera <hfznuevo@gmail.com>
2023-02-03 12:53:32 +0100

    Commit del dia, subo el proyecto por primera vez commit local en verdad segundo commit local en vardad turno del refactor

--- src/Hola_Mundo.java
+++ src/Hola_Mundo.java
@@ -2,5 +2,6 @@ public class Hola_Mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Commit locar");
+        //System.out.println("Comentario");
     }
 }
